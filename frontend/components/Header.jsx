67f16aa6d62f34dfaeb6ca73a172fd150380d967
nikhilpn725 <nikhilpn725@gmail.com>
2023-04-11 00:41:40 +0530

    1st Part of header mobile and desktop complete

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
+
+import { BsCart } from "react-icons/bs";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+import MenuMobile from "./MenuMobile";
 
 const Header = () => {
-  return <div className="">Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform divide-gray-300 ${show}`}
+    >
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href="/">
+          {/* PNG LOGO Below */}
+          {/* <img
+            src="assets/h3liosTransparent svg.png"
+            alt="main logo"
+            className="w-[40px] md:w-[120px]"
+          /> */}
+
+          <img
+            src="assets/h3liosSvg.svg"
+            alt="main logo"
+            className="w-[80px] md:w-[120px]"
+          />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}></Menu>
+
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+          ></MenuMobile>
+        )}
+
+        <div className="flex items-center gap-2 text-black">
+          {/* Icon start */}
+          <section className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[14px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              2
+            </div>{" "}
+          </section>
+          {/* Icon End */}
+
+          {/* Icon start */}
+          <section className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[14px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>{" "}
+          </section>
+          {/* Icon End */}
+
+          {/* Mobile icon start */}
+          <section className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </section>
+          {/* Mobile icon End */}
+        </div>
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
