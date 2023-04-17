9f9b9b94774e7a8d6314eaa438dd3c151adb8a19
nikhilpn725 <nikhilpn725@gmail.com>
2023-04-17 20:28:48 +0530

    category page data fetching complete product

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -1,29 +1,60 @@
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
 import React from "react";
 
-const Category = () => {
+const Category = ({ category, products, slug }) => {
   return (
     <div className="w-full md:py-20">
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Running Shoes
+            {category?.data[0]?.attributes?.name}
           </div>
         </div>
+        {/* products grid start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5  md:px-0">
+          {/* <ProductCard></ProductCard> */}
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product}></ProductCard>
+          ))}
+        </div>
       </Wrapper>
-      {/* products grid start */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5  md:px-0">
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-      </div>
       {/* products grid end */}
     </div>
   );
 };
 
 export default Category;
+
+export async function getStaticPaths() {
+  const category = await fetchDataFromApi("/api/categories?populate=*");
+  const paths = category?.data?.map((c) => ({
+    params: {
+      slug: c.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params: { slug } }) {
+  const category = await fetchDataFromApi(
+    `/api/categories?filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+  );
+
+  return {
+    props: {
+      category,
+      products,
+      slug,
+    },
+  };
+}
