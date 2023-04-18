20c8d2ee1f49601144e27b83323d8ac057aab4ac
nikhilpn725 <nikhilpn725@gmail.com>
2023-04-18 17:42:33 +0530

    cart func fully compelted by crud op done

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -2,84 +2,96 @@ import CartItem from "@/components/CartItem";
 import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
 
 const cart = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const subtotal = useMemo(() => {
+    return cartItems.reduce((total, val) => total + val.attributes.price, 0);
+  }, [cartItems]);
+
   return (
     <div className="w-full md:py-20">
       <Wrapper>
-        {/* HEADING AND PARAGRAPH START */}
-        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Shopping Cart
-          </div>
-        </div>
-        {/* HEADING AND PARAGRAPH END */}
-
-        {/* CART CONTENT START */}
-        <section className="flex flex-col lg:flex-row gap-12 py-10">
-          {/* CART ITEMS START */}
-          <div className="flex-[2]">
-            <div className="text-lg font-bold">Cart Items</div>
-            <CartItem></CartItem>
-            <CartItem></CartItem>
-            <CartItem></CartItem>
-            <CartItem></CartItem>
-          </div>
-          {/* CART ITEMS END */}
-
-          {/* SUMMART START */}
-          <div className="flex-[1]">
-            <div className="text-lg font-bold">Summary</div>
-            <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-              <div className="flex justify-between">
-                <div className="uppercase text-md md:text-lg font-medium text-black">
-                  SubTotal
-                </div>
-                <div className="text-md md:text-lg font-medium text-black">
-                  $ 19 695.00
-                </div>
-              </div>
-              <div className="text-sm md:text-md py-5 border-t mt-5">
-                The subtotal reflects the total price of your order, including
-                duties and taxes, before any applicable discounts. It does not
-                include delivery costs and international transaction fees.
+        {cartItems.length > 0 && (
+          <>
+            {/* HEADING AND PARAGRAPH START */}
+            <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+              <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                Shopping Cart
               </div>
             </div>
+            {/* HEADING AND PARAGRAPH END */}
 
-            {/* BUTTON START */}
-            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center">
-              Checkout
-            </button>
-            {/* BUTTON END */}
-          </div>
-          {/* SUMMART END */}
-        </section>
+            {/* CART CONTENT START */}
+            <section className="flex flex-col lg:flex-row gap-12 py-10">
+              {/* CART ITEMS START */}
+              <div className="flex-[2]">
+                <div className="text-lg font-bold">Cart Items</div>
+                {cartItems.map((item, index) => (
+                  <CartItem key={item.id} data={item}></CartItem>
+                ))}
+              </div>
+              {/* CART ITEMS END */}
 
-        {/* CART CONTENT START */}
+              {/* SUMMART START */}
+              <div className="flex-[1]">
+                <div className="text-lg font-bold">Summary</div>
+                <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+                  <div className="flex justify-between">
+                    <div className="uppercase text-md md:text-lg font-medium text-black">
+                      SubTotal
+                    </div>
+                    <div className="text-md md:text-lg font-medium text-black">
+                      &#36;{subtotal}
+                    </div>
+                  </div>
+                  <div className="text-sm md:text-md py-5 border-t mt-5">
+                    The subtotal reflects the total price of your order,
+                    including duties and taxes, before any applicable discounts.
+                    It does not include delivery costs and international
+                    transaction fees.
+                  </div>
+                </div>
+
+                {/* BUTTON START */}
+                <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center">
+                  Checkout
+                </button>
+                {/* BUTTON END */}
+              </div>
+              {/* SUMMART END */}
+            </section>
+            {/* CART CONTENT START */}
+          </>
+        )}
 
         {/* THIS IS EMPTY SCREEN */}
 
-        {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
-          <Image
-            src="/assets/empty-cart.jpg"
-            width={300}
-            height={300}
-            className="w-[300px] md:w-[400px]"
-          />
-          <span className="text-xl font-bold">Your cart is empty</span>
-          <span className="text-center mt-4">
-            Looks like you have not added anything in your cart.
-            <br />
-            Go ahead and explore top categories.
-          </span>
-          <Link
-            href="/"
-            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
-          >
-            Continue Shopping
-          </Link>
-        </div> */}
+        {cartItems.length < 1 && (
+          <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+            <Image
+              src="/assets/empty-cart.jpg"
+              width={300}
+              height={300}
+              className="w-[300px] md:w-[400px]"
+            />
+            <span className="text-xl font-bold">Your cart is empty</span>
+            <span className="text-center mt-4">
+              Looks like you have not added anything in your cart.
+              <br />
+              Go ahead and explore top categories.
+            </span>
+            <Link
+              href="/"
+              className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+            >
+              Continue Shopping
+            </Link>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
