7bc45f8e5c7844ff7e2c0d07576dd7d7978facda
nikhilpn725 <nikhilpn725@gmail.com>
2023-04-18 12:14:46 +0530

    product detail page data fetching and mapping complete

--- frontend/components/ProductDetailsCarousel.jsx
+++ frontend/components/ProductDetailsCarousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
-const ProductDetailsCarousel = () => {
+const ProductDetailsCarousel = ({ images }) => {
   return (
     <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
       <Carousel
@@ -12,12 +12,14 @@ const ProductDetailsCarousel = () => {
         thumbWidth={60}
         className="productCarousel"
       >
-        <img src="/assets/p2.png" />
-        <img src="/assets/p3.png" />
-        <img src="/assets/p4.png" />
-        <img src="/assets/p5.png" />
-        <img src="/assets/p6.png" />
-        <img src="/assets/p7.png" />
+        {images?.map((img) => (
+          <img
+            key={img.id}
+            alt={img.attributes.name}
+            src={img.attributes.url}
+          ></img>
+        ))}
+        {/* <img src="/assets/p7.png" /> */}
       </Carousel>
     </div>
   );
--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -1,17 +1,26 @@
 import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
 import Wrapper from "@/components/Wrapper";
 import { IoMdHeartEmpty } from "react-icons/io";
-import React from "react";
+import React, { useState } from "react";
 import RelatedProducts from "@/components/RelatedProducts";
+import { fetchDataFromApi } from "@/utils/api";
+import { getDiscountedPricePercentage } from "@/utils/helper";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const [selectedSize, setSelectedSize] = useState();
+  const [showError, setShowError] = useState(true);
+
+  const p = product?.data?.[0]?.attributes;
+  console.log(p, "p");
   return (
     <div className="w-full md:py-20">
       <Wrapper>
         <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
           {/* left column start */}
           <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
-            <ProductDetailsCarousel></ProductDetailsCarousel>
+            <ProductDetailsCarousel
+              images={p.image.data}
+            ></ProductDetailsCarousel>
           </div>
           {/* left column end */}
 
@@ -19,17 +28,27 @@ const ProductDetails = () => {
           <div className="flex-[1] py-3">
             {/* PRODUCT TITLE */}
             <div className="text-[34px] font-semibold mb-2 leading-tight">
-              Jordan Retro 6G
+              {p.name}
             </div>
 
             {/* PRODUCT SUBTITLE */}
-            <div className="text-lg font-semibold mb-5">
-              Men&apos;s Gold Shoes
-            </div>
+            <div className="text-lg font-semibold mb-5">{p.subtitle}</div>
 
             {/* PRODUCT PRICE */}
             <div className="flex items-center">
-              <p className="mr-2 text-lg font-semibold">MRP : $17 687.00</p>
+              <p className="mr-2 text-lg font-semibold">&#36; {p.price}</p>
+
+              {p.original_price && (
+                <>
+                  <p className="text-base  font-medium line-through">
+                    &#36;{p.original_price}
+                  </p>
+                  <p className="ml-auto text-base font-medium text-green-500">
+                    {getDiscountedPricePercentage(p.original_price, p.price)}%
+                    off
+                  </p>
+                </>
+              )}
             </div>
 
             <div className="text-md font-medium text-black/[0.5]">
@@ -51,39 +70,52 @@ const ProductDetails = () => {
               {/* HEADING END */}
 
               {/* SIZE START */}
-              <div className="grid grid-cols-3 gap-2">
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 5
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 7
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 8
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 9
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 10
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
-                  UK 12
-                </div>
+              <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+                {p.size.data.map((item, i) => (
+                  <div
+                    key={i}
+                    className={`border rounded-md text-center py-3 font-medium ${
+                      item.enabled
+                        ? `hover:border-black cursor-pointer`
+                        : "cursor-not-allowed bg-black/[0.1] opacity-50"
+                    } ${selectedSize === item.size ? "border-black" : ""}`}
+                    onClick={() => {
+                      if (item.enabled) {
+                        setSelectedSize(item.size);
+                        setShowError(false);
+                      }
+                    }}
+                  >
+                    {item.size}
+                  </div>
+                ))}
               </div>
               {/* SIZE END */}
 
               {/* SHOW ERROR START */}
 
-              <div className="text-red-600 mt-1">
-                Size selection is required
-              </div>
+              {showError && (
+                <div className="text-red-600 mt-1">
+                  Size selection is required
+                </div>
+              )}
               {/* SHOW ERROR END */}
             </div>
             {/* PRODUCT SIZE RANGE END */}
 
             {/* ADD TO CART BUTTON START */}
-            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
+            <button
+              className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
+              onClick={() => {
+                if (!selectedSize) {
+                  setShowError(true);
+                  document.getElementById("sizesGrid").scrollIntoView({
+                    block: "center",
+                    behavior: "smooth",
+                  });
+                }
+              }}
+            >
               Add to Cart
             </button>
             {/* ADD TO CART BUTTON END */}
@@ -120,10 +152,40 @@ const ProductDetails = () => {
           </div>
           {/* right column end */}
         </div>
-        <RelatedProducts></RelatedProducts>
+        {/* <RelatedProducts></RelatedProducts> */}
       </Wrapper>
     </div>
   );
 };
 
 export default ProductDetails;
+
+export async function getStaticPaths() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+  const paths = products?.data?.map((p) => ({
+    params: {
+      slug: p.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params: { slug } }) {
+  const product = await fetchDataFromApi(
+    `/api/products?populate=*&filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&[filters][slug][$ne]=${slug}`
+  );
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
